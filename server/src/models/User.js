8047114f6c674973c8add7b21a449554f65645df
Megan <von.poser.m@gmail.com>
2018-10-27 15:53:19 +0200

    Initial AuthenticationController

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,10 +1,12 @@
 module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+{
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       unique: true
     },
-    // password: DataTypes.STRING
     password: { type: DataTypes.STRING },
     username: { type: DataTypes.STRING }
   })
+  return User
+}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,12 +1,10 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
 module.exports = (app) => {
     app.get('/health-check', (req, res) => {
     res.send({
         message: `Server is running on port ${process.env.PORT || 8081}`
     })
 })
-    app.post('/register', (req,res) => {
-        res.send({
-            message: `You have been registered with ${req.body.username} as your username and ${req.body.email} as your email`
-        })
-    })
+
+    app.post('/register', AuthenticationController.register)
 }
